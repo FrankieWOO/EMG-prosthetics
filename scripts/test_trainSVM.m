7f69906a47b23b8e608b1adede3c3bcb9f76fce0
Fan <frankie.wufan@gmail.com>
2017-02-10 06:29:11 +0000

    change script
    change training script

--- scripts/test_trainSVM.m
+++ scripts/test_trainSVM.m
@@ -1,15 +1,23 @@
 %%  set username and movement
-user = 'Sam';
+user = 'Fan';
 movement = 'fist'; 
-filename = ['data/' user '/train/' point '.csv'];
+trialNo = [1,2,3,4];
 %% plot and choose TH
-data = readtable(filename);
-msa = msavg(table2array(data),200);
-plot(msa)
+nfiles = length(trialNo);
+dataset = cell(nfiles,1); msa = cell(nfiles,1);data=[];
+for i=1:nfiles
+filename = ['data/' user '/record/' movement '_trial' trialNo(i) '.csv'];
+dataset{i} = readtable(filename);
+msa{i} = msavg(table2array(dataset{i}),200);
+data = cat(1,data,table2array(dataset{i}));
+figure
+plot(msa{i})
+end
+
 %% Choose TH and do segmentation
 % set TH by observing the plot
 TH = 0.5 ;
 segments = emgSegment(data,TH);
 
 %%
-save(['data/' user '/train/' point '.mat'],segments)
+save(['data/' user '/train/' movement '.mat'],segments)
