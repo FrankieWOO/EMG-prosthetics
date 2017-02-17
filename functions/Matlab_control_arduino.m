0cc0b3c22440d318c49636a4c16a50f3882d97a4
Approximetal <approximetal@gmail.com>
2017-02-17 22:14:17 +0000

    Update Matlab_control_arduino.m

--- functions/Matlab_control_arduino.m
+++ functions/Matlab_control_arduino.m
@@ -2,12 +2,13 @@
    %Noticing: this program is suggested not to run directly.
    %The specific tutorial can be acquired by searching 'Control Servo Motors' in Mathwork.
 
-%% Initialization
+%% Initialization 
 a = arduino('COM9','uno'); % Connecting with the Arduinoboard.
 index = servo(a,'D10','MinPulseDuration',1*10^-3,'MaxPulseDuration',2*10^-3); % Configure the pin
 middle = servo(a,'D3','MinPulseDuration',1*10^-3,'MaxPulseDuration',2*10^-3);% The values of Max and Min are based on datasheet.
 thumb = servo(a,'D9','MinPulseDuration',1*10^-3,'MaxPulseDuration',2*10^-3); 
-%% Operations
+
+%% Operations 
 for angle = 0.2:0.1:0.8 % The area is between 0 and 1. According to the 3D-hand in lab, it should between 0.2 and 0.8.
                         % Thumb is arround 0.4 and 0.6, the structure seems has some problem.
     writePosition(middle,angle); % Drive the motors.
@@ -16,6 +17,7 @@ for angle = 0.2:0.1:0.8 % The area is between 0 and 1. According to the 3D-hand 
     fprintf('Current motor position is %d degrees\n',current_pos);
     pause(2); % Pause 2 seconds.
 end
-%% Close all (COM)
+
+%% Close all (COM 
 %delete(instrfindall);
 %clear s a
